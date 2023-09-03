b7cb29964e4ba5c415513a866e18663eddd20065
Maximilian Richt <maxi@richt.name>
2023-09-03 16:51:54 +0200

    use tileconfig instead of global one

--- tile.lua
+++ tile.lua
@@ -8,11 +8,9 @@ function parse_rgb(hex)
     return {tonumber("0x"..hex:sub(1,2))/255, tonumber("0x"..hex:sub(3,4))/255, tonumber("0x"..hex:sub(5,6))/255}
 end
 
-function M.updated_config_json(config)
-    clean_topic = config.mqtt_topic:gsub("[^A-Za-z0-9]", "_")
-end
+function M.task(starts, ends, tileconfig, x1, y1, x2, y2)
+    clean_topic = tileconfig.mqtt_topic:gsub("[^A-Za-z0-9]", "_")
 
-function M.task(starts, ends, config, x1, y1, x2, y2)
     return bg:draw(x1, y1, x2, y2)
 end
 
@@ -26,8 +24,8 @@ function M.data_trigger(path, data)
     bg = resource.create_colored_texture(color[1], color[2], color[3], 1)
 end
 
-function M.can_show(config)
-    if clean_topic == "" then
+function M.can_show(tileconfig)
+    if tileconfig.mqtt_topic == "" then
         return false
     else
         return true
