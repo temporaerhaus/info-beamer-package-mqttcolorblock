242648fb61c8ebb93c0d1f7ded606a825dc997de
robbi5 <maxi@richt.name>
2024-04-28 21:41:48 +0200

    a bit more logging inside the tile

--- tile.lua
+++ tile.lua
@@ -10,6 +10,7 @@ end
 
 function M.task(starts, ends, tileconfig, x1, y1, x2, y2)
     clean_topic = tileconfig.mqtt_topic:gsub("[^A-Za-z0-9]", "_")
+    print("now listening for path", "root/plugin/colorblock/" .. clean_topic)
 
     return bg:draw(x1, y1, x2, y2)
 end
@@ -17,9 +18,12 @@ end
 function M.data_trigger(path, data)
     local my_path = "root/plugin/colorblock/" .. clean_topic
     if path ~= my_path then
+        print("ignoring data on path", path)
         return
     end
+    print("got color update", data)
     color = parse_rgb(data)
+    print("thats color", color)
     bg:dispose()
     bg = resource.create_colored_texture(color[1], color[2], color[3], 1)
 end
