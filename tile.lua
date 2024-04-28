9697402b9712719dede243155bfbbc17fca169b3
robbi5 <maxi@richt.name>
2024-04-28 21:48:53 +0200

    lua: color should be local, lets try to listen to config updates

--- tile.lua
+++ tile.lua
@@ -15,13 +15,20 @@ function M.task(starts, ends, tileconfig, x1, y1, x2, y2)
     return bg:draw(x1, y1, x2, y2)
 end
 
+function M.updated_config_json(config)
+    pp(config)
+
+    local new_clean_topic = config.mqtt_topic:gsub("[^A-Za-z0-9]", "_")
+    print("got config update, listening for path", new_clean_topic)
+end 
+
 function M.data_trigger(path, data)
     if path ~= clean_topic then
         print("ignoring data on path", path)
         return
     end
     print("got color update", data)
-    color = parse_rgb(data)
+    local color = parse_rgb(data)
     print("thats color", color)
     bg:dispose()
     bg = resource.create_colored_texture(color[1], color[2], color[3], 1)
