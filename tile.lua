b0b7f56dfb8cc953e678f87356d5d734ca871515
robbi5 <maxi@richt.name>
2024-04-28 21:44:56 +0200

    oh, tile.lua already gets data on cleaned subpath

--- tile.lua
+++ tile.lua
@@ -10,14 +10,13 @@ end
 
 function M.task(starts, ends, tileconfig, x1, y1, x2, y2)
     clean_topic = tileconfig.mqtt_topic:gsub("[^A-Za-z0-9]", "_")
-    print("now listening for path", "root/plugin/colorblock/" .. clean_topic)
+    print("now listening for path", clean_topic)
 
     return bg:draw(x1, y1, x2, y2)
 end
 
 function M.data_trigger(path, data)
-    local my_path = "root/plugin/colorblock/" .. clean_topic
-    if path ~= my_path then
+    if path ~= clean_topic then
         print("ignoring data on path", path)
         return
     end
